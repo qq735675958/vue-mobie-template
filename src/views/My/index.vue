0b2373c4c89edfa56a68a8f0b136ec3e3d3ac591
huyu <qq735675958@qq.com>
2019-12-24 19:28:39 +0800

    新增输入框

--- src/views/My/index.vue
+++ src/views/My/index.vue
@@ -1,6 +1,39 @@
 <template>
   <div class="my">
-    <van-nav-bar title="我的" left-text="返回" right-text="按钮" left-arrow />
+    <van-nav-bar title="充值" left-text="返回" right-text="按钮" left-arrow />
+    <van-row>
+      <van-col span="12">
+        <p class="investMoney">充值金额</p>
+      </van-col>
+    </van-row>
+    <van-row>
+      <van-col span="12">
+        <van-cell-group>
+          <van-field
+            ref="inputenter"
+            :autofocus="autofocus"
+            @input="InputValue"
+            v-model="value"
+            @focus="focusNum"
+            type="number"
+            placeholder="请输入用户名"
+          />
+          <!-- <input type="text"
+            @focus="focusNum"
+            id="aaa"
+            v-model="value" /> -->
+        </van-cell-group>
+      </van-col>
+    </van-row>
+    <van-number-keyboard
+      :show="show"
+      extra-key="."
+      close-button-text="完成"
+      @blur="show = true"
+      v-model="value"
+      @input="onInput"
+      @delete="onDelete"
+    />
   </div>
 </template>
 <script>
@@ -8,9 +41,35 @@ export default {
   name: "my",
   data() {
     return {
-      show: true
+      autofocus: true,
+      show: true,
+      value: "",
+      value1: ""
     };
   },
-  methods: {}
+  methods: {
+    focusNum() {
+      //   document.activeElement.blur();
+    },
+    InputValue(val) {
+      //   console.log(val);
+      console.log(val.length);
+    },
+    onInput(val) {
+      console.log(val);
+    },
+    onDelete(val) {
+      this.value = this.value.slice(0, this.value.length - 1);
+      this.$refs.inputenter.focus();
+      console.log(this.$refs.inputenter);
+      this.$refs.inputenter.focus();
+    }
+  }
 };
 </script>
+<style lang="less" scoped>
+.investMoney {
+  padding: 10px 0;
+  font-size: 14px;
+}
+</style>
